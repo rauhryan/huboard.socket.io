6d1b3716a7ee08ff18ffaae8329fbdcf29cfcb1a
Ryan Rauh <rauh.ryan@gmail.com>
2012-05-19 12:50:03 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+app = express.createServer(),
+io = require("socket.io").listen(app);
+
+app.configure(function(){
+  app.use(express.bodyParser());
+  app.use(app.router);
+});
+
+io.configure(function() {
+   io.set("transports", ["xhr-polling"]);
+   io.set("pull duration", 10);
+});
+
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+app.post("/hook", function (req, res) {
+  io.sockets.emit(req.body.channel, req.body.payload);
+  res.send({});
+});
+
+app.listen(8080);
