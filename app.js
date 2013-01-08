1a0a3bc25222ea81c694e0d9bb97652e7a057261
Ryan Rauh <rauh.ryan@gmail.com>
2013-01-08 09:18:43 -0600

    moving the web hook to node

--- app.js
+++ app.js
@@ -17,6 +17,24 @@ app.get('/', function(req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
+app.post("issues/webhook", function(req,res) {
+   var issue = req.body.payload.issue;
+
+   issue._data = {};
+   issue.repo = req.body.payload.repository;
+   issue.other_labels = [];
+
+   switch(req.body.payload.action){
+      case "opened":
+        io.sockets.emit(req.body.payload.repository.full_name, {payload:issue,event:"Opened.0"});
+        break;
+      case "closed":
+        io.sockets.emit(req.body.payload.repository.full_name, {payload:issue,event:"Closed." + issue.number});
+        break;
+
+   }
+});
+
 app.post("/hook", function (req, res) {
   req.body.secret === process.env.SECRET && io.sockets.emit(req.body.channel, req.body.payload);
   res.send({});
